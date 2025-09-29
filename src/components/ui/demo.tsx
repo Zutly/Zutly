55f11e646e3570863bc683880dcd5bd741aa8c28
[dyad] <contact@ailab.courses>
2025-09-29 15:57:01 +0200

    Reverted all changes back to version e1d72e60f9f9a13a0aa158b00267644f59f3a4dc

--- src/components/ui/demo.tsx
+++ src/components/ui/demo.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { GooeyText } from "@/components/ui/gooey-text-morphing";
-import { Typewriter } from "@/components/ui/typewriter-text";
 
 function GooeyTextDemo() {
   return (
@@ -15,17 +14,4 @@ function GooeyTextDemo() {
   );
 }
 
-const DemoVariant1 = () => {
-  return (
-    <>
-      <Typewriter
-        text={["Welkom bij Zutly", "Uw partner voor innovatie weboplossingen", "en digitale transformatie."]}
-        speed={100}
-        loop={true}
-        className="text-xl font-medium"
-      />
-    </>
-  )
-}
-
-export { GooeyTextDemo, DemoVariant1 };
+export { GooeyTextDemo };
